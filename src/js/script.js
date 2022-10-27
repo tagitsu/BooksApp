39b20575b61f210117b1e3c1eba780e2ef057568
Julia Diaków <tagitsu@gmail.com>
2022-10-27 13:24:39 +0200

    Add function that adds book to favorites array - 9.2 ćwiczenie 2

--- src/js/script.js
+++ src/js/script.js
@@ -13,7 +13,8 @@ const select = {
   }, 
   books: {
     list: '.books-list',
-    book: '.book'
+    book: '.book',
+    bookLink: 'a.book__image',
   },
   templateOf: {
     book: '#template-book',
@@ -24,15 +25,34 @@ const templates = {
   bookPanel: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
 };
 
-function initBooksList() {
+function renderBooksList() {
   for (let book of this.data.books) {
     const codeHTML = templates.bookPanel(book);
     const domElement = utils.createDOMFromHTML(codeHTML);
     const bookList = document.querySelector(select.books.list);
     bookList.appendChild(domElement);
   }
-
-
 }
 
-initBooksList();
+renderBooksList();
+
+// Ćwiczenie 2
+// musisz dodać do JS-a pustą tablicę favoriteBooks oraz odpowiedni kod, który zadba 
+// o to, aby przy dwukliku na okładkę dowolnej książki, jej id było dodawane do tej tablicy, 
+// a do samego elementu HTML została dodana klasa favorite. Tyle.
+function addToFavorites() {
+  const favoritesBooks = [];
+  favoritesBooks.push(this);
+  console.log('ulubione', favoritesBooks);
+  this.classList.add('favorite');
+}
+function initAction() {
+  const bookLink = document.querySelectorAll(select.books.bookLink);
+  console.log('linki do książek', bookLink);
+  for (let link of bookLink) {
+    link.addEventListener('dblclick', addToFavorites);
+  }
+}
+
+
+initAction();
